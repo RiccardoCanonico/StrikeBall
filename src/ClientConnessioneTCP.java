fe73bf9af0407852a2082cf91e08814ec2803a91
RiccardoCanonico <riccardocanonico2001@gmail.com>
2020-02-03 09:58:25 +0100

    comunicazione

--- src/ClientConnessioneTCP.java
+++ src/ClientConnessioneTCP.java
@@ -4,7 +4,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.net.ConnectException;
@@ -17,6 +20,8 @@ public class ClientConnessioneTCP {
     /**
      * @param args the command line arguments
      */
+    static String mess = "mr.sandman";
+    
     public static void main(String[] args){
         //oggetto da usare per realizzare la connessione TCP
         Socket connection = null;
@@ -24,11 +29,20 @@ public class ClientConnessioneTCP {
         String serverAddress = "localhost";
         //porta del server in ascolto
         int port = 2000;
-
+        DataOutputStream out;
+        BufferedReader in;
         //apertura della connessione al server sulla porta specificata
         try{
             connection = new Socket(serverAddress, port);
             System.out.println("Connessione aperta");
+            out = new DataOutputStream(connection.getOutputStream());
+            out.writeBytes(mess);
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            
+            String str = in.readLine();
+            System.out.println(str);
+            
+            out.close();
         }
         catch(ConnectException e){
             System.err.println("Server non disponibile!");
--- src/ServerConnessioneTCP.java
+++ src/ServerConnessioneTCP.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -28,31 +31,44 @@ public class ServerConnessioneTCP {
         Socket connection;
         int timer=10000;
         Countdown c=new Countdown(timer);
-            try{
-                // il server si mette in ascolto sulla porta voluta
-                sSocket = new ServerSocket(port);
-                System.out.println("In attesa di connessioni!");
-                sSocket.setSoTimeout(timer);
-                c.start();
-                connection = sSocket.accept();
-                c.connected=true;
-                System.out.println("Connessione stabilita!");
-                System.out.println("Socket server: " + connection.getLocalSocketAddress());
-                System.out.println("Socket client: " + connection.getRemoteSocketAddress());
-                c.interrupt();
-            }
-            catch(IOException e){
-                System.err.println("Errore di I/O!");
-            }
+        BufferedReader in;
+        DataOutputStream out;
+        try{
+            // il server si mette in ascolto sulla porta voluta
+            sSocket = new ServerSocket(port);
+            System.out.println("In attesa di connessioni!");
+            sSocket.setSoTimeout(timer);
+            c.start();
+            connection = sSocket.accept();
+            c.connected=true;
+            System.out.println("Connessione stabilita!");
+            System.out.println("Socket server: " + connection.getLocalSocketAddress());
+            System.out.println("Socket client: " + connection.getRemoteSocketAddress());
+            c.interrupt();
+            
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            out = new DataOutputStream(connection.getOutputStream());
+            
+            String str = in.readLine();
+            System.out.println(str);
+            
+            String strRisp = "ciao";
+            out.writeBytes(strRisp);
+            
+            out.close();
+        }
+        catch(IOException e){
+            System.err.println("Errore di I/O!");
+        }
 
-            //chiusura della connessione con il client
-            try {
-                if (sSocket!=null) 
-                    sSocket.close();
-            } 
-            catch (IOException ex) {
-                System.err.println("Errore nella chiusura della connessione!");
-            }
-            System.out.println("Connessione chiusa!");
+        //chiusura della connessione con il client
+        try {
+            if (sSocket!=null) 
+                sSocket.close();
+        } 
+        catch (IOException ex) {
+            System.err.println("Errore nella chiusura della connessione!");
+        }
+        System.out.println("Connessione chiusa!");
       }
 }
